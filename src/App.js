06f36f2132ce80bb24d962ce3db9939030753d6e
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-07-20 16:10:37 -0700

    displayed data, user is able to sort and search data by car type car name car year

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import DataList from './DataList'
+const App = () => {
+  return (
+    <div>
+      <DataList />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/DataList.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Data from './Data.json'
+import BootstrapTable from 'react-bootstrap-table-next'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.css'
+import paginationFactory from 'react-bootstrap-table2-paginator'
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css'
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter'
+
+const DataList = () => {
+    // boostrap table to display data
+    const columns = [
+        {dataField:'Make_ID', text: 'Make ID',filter: textFilter(), sort: true, },
+        {dataField:'Make_Name', text: 'Make Name',filter: textFilter(), sort: true, },
+        {dataField:'Model_ID', text: 'Model ID',filter: textFilter(), sort: true,  },
+        {dataField:'Model_Name', text: 'Model Name',filter: textFilter(), sort: true},
+        { dataField:'VehicleTypeName' , text: 'Vehicle Type',filter: textFilter(), sort: true},
+        { dataField:'Model_Year', text: '2015', sort: true,filter: textFilter()  }
+    ]
+    // boostrap paginatation 
+    const pagination = paginationFactory({
+        page: 1,
+        sizePerPage: 30,
+        lastPageText: '>>',
+        firstPageText:'<<',
+        nextPageText: '>',
+        prePageText: '<',
+        showTotal: true,
+        alwaysShowAllBtns: true,
+        onPageChange: function(page, sizePerPage){
+            console.log('page', page);
+            console.log('sizePerPage', sizePerPage)
+
+        },
+        onSizePerPageChange: function(page, sizePerPage){
+            console.log('page', page);
+            console.log('sizePerPage', sizePerPage)
+        }
+    })
+    return (
+        <div>
+            <BootstrapTable 
+                bootstrap4 
+                keyField="Make_ID" 
+                columns={columns} 
+                data={Data.Results}  
+                pagination={pagination} 
+                filter={filterFactory()}
+                striped
+                condensed
+                hover/>
+        </div>
+    )
+}
+
+export default DataList
